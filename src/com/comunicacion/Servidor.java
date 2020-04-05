eb7b30b1afa14e6ff8566c2e2cabc35d50a89c95
HectorRodriguez-hub <53011613+HectorRodriguez-hub@users.noreply.github.com>
2020-04-05 16:23:10 -0500

    Cara fea

--- src/com/comunicacion/Servidor.java
+++ src/com/comunicacion/Servidor.java
@@ -28,6 +28,6 @@ public class Servidor
 		ServerSocket ss = new ServerSocket(4999);
 		/*Aceptando solicitud del cliente*/
 		Socket sc = ss.accept(); 
-		System.out.println("Un cliente se ah conectado :)!");
+		System.out.println("Un cliente se ha conectado !");
 	}
 }

7824acaaf6e95386b6227f53893e7b9eef82537b
HectorRodriguez-hub <53011613+HectorRodriguez-hub@users.noreply.github.com>
2020-05-22 12:01:47 -0500

    Update Cita.java
    atirbutos cita

--- Proyecto Asignación de citas por COVID/src/com/negocio/Cita.java
+++ Proyecto Asignación de citas por COVID/src/com/negocio/Cita.java
@@ -1,6 +1,15 @@
 package com.negocio;
 
-public class Cita
-{
+public class Cita {
+	private boolean autorizacion;
+	private String fecha;
+	private int identificador;
 
+	public Cita(boolean autorizacion, String fecha, int identificador) {
+		super();
+		this.autorizacion = autorizacion;
+		this.fecha = fecha;
+		this.identificador = identificador;
+	}
+	
 }

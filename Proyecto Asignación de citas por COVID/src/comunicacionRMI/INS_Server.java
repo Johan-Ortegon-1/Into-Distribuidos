5099c4d93e774309b3017e5ada132aab2bf7f7b3
Johan Ortegon 1 <46930696+Johan-Ortegon-1@users.noreply.github.com>
2020-05-24 18:30:48 -0500

    dgfhj

--- Proyecto Asignación de citas por COVID/src/comunicacionRMI/INS_Server.java
+++ Proyecto Asignación de citas por COVID/src/comunicacionRMI/INS_Server.java
@@ -1,10 +1,11 @@
 package comunicacionRMI;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import com.negocio.Paciente;
 
-public interface INS_Server
+public interface INS_Server extends Remote
 {
 	public int responderPeticionPuntaje(Paciente pacienteActual)throws RemoteException;
 }
